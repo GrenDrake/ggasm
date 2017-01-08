3f09fc147e798af1677834cf47ac54ac9387ab06
Gren Drake <grendrake@gmail.com>
2017-01-08 08:32:09 -0800

    Added nearly complete port of model.c as example

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #! /bin/sh
+echo "Building basic.asm"
 java -jar ./build/libs/GGASM-1.0.jar asmFiles/basic.asm asmFiles/basic.ulx
-# echo $?
+echo "Building model.asm"
+java -jar ./build/libs/GGASM-1.0.jar asmFiles/model.asm asmFiles/model.ulx
